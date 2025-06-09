c77875eebbfe831aea91cafba75bcd993dd09bd7
ANGADSINGHT <angadpaltak@outlook.com>
2025-06-09 20:17:04 +0100

    Actually fixed the error reported

--- cogs/apply/server_type_callback.py
+++ cogs/apply/server_type_callback.py
@@ -1,4 +1,4 @@
-from nextcord import Interaction
+from nextcord import Interaction, TextInputStyle, ui
 
 async def ServerTypeCallback(interaction: Interaction) -> None:
 	application_details[interaction.user.id].append(interaction.data["values"][0])
@@ -7,19 +7,19 @@ async def ServerTypeCallback(interaction: Interaction) -> None:
 		title="Server Application",
 		timeout=None
 	)
-	modal.add_item(nextcord.ui.TextInput(
+	modal.add_item(ui.TextInput(
 		label="Why do you want a server?",
 		placeholder="Explain your motivation...",
 		required=True,
-		style=nextcord.TextInputStyle.paragraph
+		style=TextInputStyle.paragraph
 	))
-	modal.add_item(nextcord.ui.TextInput(
+	modal.add_item(ui.TextInput(
 		label="How did you find us?",
 		placeholder="Describe your connection...",
 		required=True,
-		style=nextcord.TextInputStyle.paragraph
+		style=TextInputStyle.paragraph
 	))
-	modal.add_item(nextcord.ui.TextInput(
+	modal.add_item(ui.TextInput(
 		label="Email",
 		placeholder="*****@*******.com",
 		required=True
